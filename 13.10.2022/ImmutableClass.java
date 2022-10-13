aa031679e5252ed8de5c2321680dc3c287487c3a
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-10-13 13:52:45 +0530

    Update and rename ImmutableClass.java to 13.10.2022/ImmutableClass.java

--- 13.10.2022/ImmutableClass.java
+++ 13.10.2022/ImmutableClass.java
@@ -1,4 +1,5 @@
-
+/*Q.3... Develop a public java class and make sure nobody can create
+any object of this class from outside the class*/
 public final class ImmutableClass 
 {
 	private String name;
