aa1b9e7d33d8deaca91829521b83205154fb54b2
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-10-14 10:58:59 +0530

    Update ImmutableClass.java

--- 13.10.2022/ImmutableClass.java
+++ 13.10.2022/ImmutableClass.java
@@ -4,30 +4,4 @@ public final class ImmutableClass
 {
 	private String name;
 	  private int date;
-
-	  ImmutableClass(String name, int date) {
-
-	    // class members are initialized using constructor
-	    this.name = name;
-	    this.date = date;
-	  }
-	  public String Name() {
-	    return name;
-	  }
-
-	  public int Date() {
-	    return date;
-	  }
-
-	}
-
-	class Main {
-	  public static void main(String[] args) {
-
-	    // create object of Immutable
-	    ImmutableClass obj = new ImmutableClass("Shifa",2022);
-
-	    System.out.println("Name: " + obj.Name());
-	    System.out.println("Date: " + obj.Date());
-	  }
 }
